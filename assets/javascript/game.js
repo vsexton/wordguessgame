73c5d9e0eb04fa5d8332fb5c205c1cb5fa257f94
majolex <majolex@matts-MacBook-Air.local>
2018-11-19 00:16:59 -0700

    added new info

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,55 +1,78 @@
 //Global variables
-const word = ['mistletoe','jinglebells','eggnog','naughty','nice','nutcracker','cookies','jolly','elves','snowman'];
+// Used to record how many times a letter can be pressed
+var doubleWord = ['a','b','c',
+				  'd','e','f',
+				  'g','h','i',
+				  'j','k','l',
+				  'm','n','o',
+				  'p','q','r',
+				  's','t','u',
+				  'v','w','x',
+                  'y','z'];
+                  
+           
+const wordBank = ['Mistletoe','noel','jinglebells','frosty','naughty','nice','nutcracker','cookies','jolly','elves','snowman'];
+let randNum = [Math.floor(Math.random() * wordBank.length)];
+let choosenWord = wordBank[randNum];
 
-let randNum = Math.floor(Math.random() * word.length);
-let choosenWord = word[randNum];
-//let word = [];
 let wrongWord = [];
 let underScore = [];
+let lettersInWord = [];
+let numBlanks = 0;
+let rightGuessCounter = 0;
+let blanksAndSuccesses =[];
+
+
 
 //Dom manipulation
 let docUnderScore = document.getElementsByClassName('underScore');
+let docRightGuess = document.getElementsByClassName('rightGuess');
 let docWrongGuess = document.getElementsByClassName('wrongGuess');
+console.log(choosenWord);
+
 //Main
 //=============================================================
+
+
+	
 
 // Split chosen word into an array
 
 
-//create underscore based on length of word
-
 //let  generateUnderScore = () => {
     for(let i = 0; i < choosenWord.length; i++){
         underScore.push('_');
-       console.log(underScore);
+       console.log(choosenWord);
     }
  //   return underScore;
 //}
- console.log(choosenWord);
+ //console.log(choosenWord);
 
 //Push underScore on to page
+
 //underScore.push(keyword);
 var stuff = document.getElementById("underScore");
 console.log(stuff);
-stuff.innerHTML("<p>Blah</p>");
+stuff.innerHTML = underScore.join(' ')
+
+//var stuff = document.getElementById("underScore");
+//console.log(stuff);
+//stuff.innerHTML("<p>Blah</p>");
 // console.log(document.getElementById("underScore"))
-
-
 //get user guess
-document.addEventListener('keypress', (event) => { 
-   let keyword = String.fromCharCode(event.keyCode);
-// if users guess is right
-   if(choosenWord.indexOf(keyword) > -1){
-//add to right words array
-    rightWord.push(keyword);
-   
 
+document.addEventListener('keypress', (event) => { 
+let keyword = String.fromCharCode(event.keyCode)// if users guess is right
+   if(choosenWord.indexOf(keyword) > -1){
+
+    //add to right words array
+   
 //replace undrscore with right letter
      underScore[choosenWord.indexOf(keyword)] = keyword; 
-     docUnderScore[0].innerHTML = underScore.join(' ');
-     rightWord[0].innerHTML = rightGuess;
+     //docUnderScore[0].innerHTML = underScore.join(' ');
+     //choosenWord[0].innerHTML = rightGuess;
 // checks to see if user word matches guesses 
-    if(underScore.join('') == choosenWord){
+    if(underScore.join('') == choosenWord) {
     alert('You Win');
    
 }
@@ -59,4 +82,19 @@ document.addEventListener('keypress', (event) => {
        docWrongGuess[0].innerHTML = wrongWord;
    } 
     });
-
+//RESET
+	//===========================================================
+	letterGuessed = 0;
+	rightGuessCounter = 0;
+	guessesLeft = 9;
+	wrongLetters =[];
+	blanksAndSuccesses =[];
+	doubleWord = ['a','b','c',
+					  'd','e','f',
+					  'g','h','i',
+					  'j','k','l',
+					  'm','n','o',
+					  'p','q','r',
+					  's','t','u',
+					  'v','w','x',
+					  'y','z'];
